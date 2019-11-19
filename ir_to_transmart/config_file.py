dc846c3cfc0a685185d00fc30ebe665be71535ac
abel <aurelien.beliard@sorbonne-universite.fr>
2019-11-19 17:15:13 +0100

    change config file

--- ir_to_transmart/config_file.py
+++ ir_to_transmart/config_file.py
@@ -1,13 +1,14 @@
 
 class config_data:
-    authorize_url = "https://transmart.i3lab.cloud:8445/transmart/oauth/authorize"
-    token_url = "https://transmart.i3lab.cloud:8445/transmart/oauth/token"
+    domain_name = "https://transmart.i3lab.cloud:8444/transmart/"
+    authorize_url = domain_name+"oauth/authorize"
+    token_url = domain_name+"oauth/token"
 
     #callback url specified when the application was defined
-    callback_uri = "https://transmart.i3lab.cloud:8445/transmart/oauth/verify"
+    callback_uri = domain_name+"oauth/verify"
 
     client_id = 'api-client'
     client_secret = 'api-client'
-    access_token = "580f05d1-06f5-44dd-8e99-f5d17aa98e26"
-    refresh_token = "0a07d7a1-3c9b-4545-9142-999e32d7a041"
-    expires_in = ""
+    access_token = "cc16f12f-b994-45d9-a0c7-faa78ffdc01d"
+    refresh_token = "2925861b-98fd-4f21-9adc-41060fd0ac11"
+    # expires_in = ""
